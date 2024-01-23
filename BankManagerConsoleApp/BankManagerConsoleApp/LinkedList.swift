cbfba9ae50fe1f0e9339186b50f74da63f83c0a7
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-23 14:40:31 +0900

    feat: LinkedList 구현 - head 객체 구현 - LinkedList에 활용할 메서드 구현(first, isEmpty, append, remove, insert)

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -5,4 +5,63 @@
 //  Created by 강창현 on 1/23/24.
 //
 
-import Foundation
+struct LinkedList<T> {
+    
+    // MARK: - Properties
+    private var head: Node<T>?
+    var first: T? {
+        return head?.data
+    }
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    init(head: Node<T>? = nil) {
+        self.head = head
+    }
+    
+    // MARK: - Methods
+    mutating func append(data: T?) {
+        if head == nil {
+            head = Node(data: data)
+        }
+        var node = head
+        while node?.next != nil {
+            node = node?.next
+        }
+        node?.next = Node(data: data)
+    }
+    
+    @discardableResult
+    mutating func remove(at index: Int) -> T? {
+        if head == nil { return nil }
+        
+        if index == 0 || head?.next == nil {
+            let removedNode = head?.data
+            head = head?.next
+            return removedNode
+        }
+        var node = head
+        for _ in 0..<(index - 1) {
+            if node?.next?.next == nil { break }
+            node = node?.next
+        }
+        let removedNode = node?.next?.data
+        node?.next = node?.next?.next
+        return removedNode
+    }
+    
+    // MARK: - Private Methods
+    mutating private func insert(data: T?, at index: Int) {
+        if head == nil {
+            head = Node(data: data)
+        }
+        var node = head
+        for _ in 0..<(index - 1) {
+            node = node?.next
+        }
+        let nextNode = node?.next
+        node?.next = Node(data: data)
+        node?.next?.next = nextNode
+    }
+}
