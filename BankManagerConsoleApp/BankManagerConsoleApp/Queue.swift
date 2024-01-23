47703fb0e71ee09d8b476b77c6954046065dac7a
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-23 14:37:31 +0900

    feat: Queue 객체 구현 - LinkedList 선언 - Queue에 필요한 메서드 구현(isEmpty, peek, enqueue, dequeue, clear)

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -5,4 +5,35 @@
 //  Created by 강창현 on 1/23/24.
 //
 
-import Foundation
+struct Queue<T> {
+    
+    // MARK: - Properties
+    private var linkedList = LinkedList<T>()
+    
+    var peek: T? {
+        return linkedList.first
+    }
+    var isEmpty: Bool {
+        return linkedList.isEmpty
+    }
+    
+    init(linkedList: LinkedList<T> = LinkedList<T>()) {
+        self.linkedList = linkedList
+    }
+    
+    // MARK: - Methods
+    mutating func enqueue(_ element: T) {
+        linkedList.append(data: element)
+    }
+    
+    mutating func dequeue() -> T? {
+        guard !linkedList.isEmpty else { return nil }
+        return linkedList.remove(at: 0)
+    }
+    
+    mutating func clear() {
+        while !linkedList.isEmpty {
+            linkedList.remove(at: 0)
+        }
+    }
+}
